ac169dc82a36d9fde1f8e27eaa9afdf720cc533e
雪猫 <SnowCait@users.noreply.github.com>
2022-05-01 11:22:03 +0900

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -4,3 +4,8 @@ winget install -e --id Docker.DockerDesktop --silent
 # Editor
 winget install -e --id Microsoft.VisualStudioCode --silent
 winget install -e --id JetBrains.Toolbox --silent
+
+# Git
+winget install --id Git.Git -e --source winget --silent
+winget install --id GitHub.cli --silent
+winget install -e --id Fork.Fork --silent
