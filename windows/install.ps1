c6e8695da7723a4ce311d0171d6788edecba9f3e
雪猫 <SnowCait@users.noreply.github.com>
2022-05-01 11:22:29 +0900

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -9,3 +9,6 @@ winget install -e --id JetBrains.Toolbox --silent
 winget install --id Git.Git -e --source winget --silent
 winget install --id GitHub.cli --silent
 winget install -e --id Fork.Fork --silent
+
+# Hashicorp
+choco install terraform
