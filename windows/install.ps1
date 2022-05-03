1539caabe7625ee6dd1fcea32202095a724ea2a6
雪猫 <SnowCait@users.noreply.github.com>
2022-05-03 14:31:17 +0900

    Git

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,14 +1,14 @@
+# Git
+winget install --id Git.Git -e --source winget --silent
+winget install --id GitHub.cli --silent
+winget install -e --id Fork.Fork --silent
+
 # Docker
 winget install -e --id Docker.DockerDesktop --silent
 
 # Editor
 winget install -e --id Microsoft.VisualStudioCode --silent
 winget install -e --id JetBrains.Toolbox --silent
-
-# Git
-winget install --id Git.Git -e --source winget --silent
-winget install --id GitHub.cli --silent
-winget install -e --id Fork.Fork --silent
 
 # Hashicorp
 choco install terraform
