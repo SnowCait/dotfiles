e206461462d23dcf05f177827541b23f83ac37d6
雪猫 <SnowCait@users.noreply.github.com>
2022-05-03 14:32:02 +0900

    GitHub Desktop

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,6 +1,7 @@
 # Git
 winget install --id Git.Git -e --source winget --silent
 winget install --id GitHub.cli --silent
+winget install -e --id GitHub.GitHubDesktop --silent
 winget install -e --id Fork.Fork --silent
 
 # Docker
