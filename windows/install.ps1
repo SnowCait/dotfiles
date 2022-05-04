ee2c951d3264cb997f2f614d30701e7201407749
雪猫 <SnowCait@users.noreply.github.com>
2022-05-05 00:21:35 +0900

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -14,8 +14,8 @@ winget install -e --id Docker.DockerDesktop --silent
 winget install -e --id Microsoft.VisualStudioCode --silent
 winget install -e --id JetBrains.Toolbox --silent
 
-# Hashicorp
-choco install terraform
+# # Hashicorp
+# choco install terraform
 
 # Node.js
 winget install -e --id OpenJS.NodeJS -v 16.12.0 --silent
