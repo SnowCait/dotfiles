67750e560c073051517e99302faea746972c790e
雪猫 <SnowCait@users.noreply.github.com>
2022-05-05 01:51:54 +0900

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -13,6 +13,7 @@ winget install -e --id Docker.DockerDesktop --silent
 # Editor
 winget install -e --id Microsoft.VisualStudioCode --silent
 winget install -e --id JetBrains.Toolbox --silent
+winget install --id=Microsoft.VisualStudio.2022.Community-Preview  -e --silent
 
 # # Hashicorp
 # choco install terraform
